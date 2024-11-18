21387d7fe50d3cf7d403bf9f84901c42252f055e
MiguelA-Izquierdo <izquierdomiguela@gmail.com>
2024-11-18 20:30:49 +0100

    Initial commit

---
+++ src/main/java/com/app/shopbackend/ShopBackEnd.java
@@ -0,0 +1,13 @@
+package com.app.shopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class ShopBackEnd {
+  public static void main(String[] args) {
+    SpringApplication.run(ShopBackEnd.class, args);
+  }
+}
